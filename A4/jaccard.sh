171946ba32cdad13977fdf8604629f3d3e0da5c1
VictorDasari <dasarivictor@gmail.com>
2015-05-02 00:09:18 -0400

    A4

---
+++ A4/jaccard.sh
@@ -0,0 +1,18 @@
+for j in $(seq 2 1000) ### Inner for loop ###
+ do
+#   i=j     
+   a=`grep -Fx -f 1.txt $j.txt | wc -w` 
+   #echo $a
+   b=`grep -Fxv -f 1.txt $j.txt | wc -w`
+   #echo $b
+   c=`grep -Fxv -f $j.txt 1.txt | wc -w`
+   echo $c
+   u=`expr $a + $b + $c`
+   echo $u
+   k=`expr $a / $u`
+   #echo $k
+   l=1
+   f=`expr $l - $k`
+   echo $f
+   #`echo $f >> final.txt`
+done
